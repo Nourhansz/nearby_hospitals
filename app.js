031b695cf441f56af7d60d653f0843e6414c4832
Nourhansz <48129640+Nourhansz@users.noreply.github.com>
2022-03-01 19:42:18 +0400

    ambulance requester and tracker

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(userRouter);
 
+app.get('/',(req,res)=>{
+    res.json("hello");
+})
+
 
 app.listen(port, ()=>{
     console.log("port is listening");
